dc2e1b87695f35e70f1504c549f10fd1cce4c654
Daniel Vacek <Raimen77@seznam.cz>
2022-10-03 00:13:12 +0200

    fixing

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import { Provider } from "react-redux";
-import { HydrationProvider, Server, Client } from "react-hydration-provider";
+
 import store from "../store/index";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <HydrationProvider>
-        <Client>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Client>
-      </HydrationProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   );
 }
