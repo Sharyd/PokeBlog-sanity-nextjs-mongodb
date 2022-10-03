36e0ebee6dc8d0c3edbf48c944f53480b0376f5f
Daniel Vacek <Raimen77@seznam.cz>
2022-10-03 02:11:48 +0200

    fixing stufs

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -14,7 +14,6 @@ const Admin = ({ messages }) => {
 
 export const getServerSideProps = async (ctx) => {
   const myCookie = ctx.req?.cookies || "";
-  let admin = false;
   if (myCookie.token !== process.env.NEXT_PUBLIC_TOKEN) {
     return {
       redirect: {
@@ -23,16 +22,12 @@ export const getServerSideProps = async (ctx) => {
       },
     };
   }
-  if (myCookie.token === process.env.NEXT_PUBLIC_TOKEN) {
-    admin = true;
-  }
 
   const messages = await fetchData(`http://localhost:3000/api/contactHandler`);
 
   return {
     props: {
       messages: messages,
-      admin: admin,
     },
   };
 };
