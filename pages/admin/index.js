258a18ce996e23c3ce520c6315f43ac5d53de68f
Daniel Vacek <Raimen77@seznam.cz>
2022-10-04 02:37:17 +0200

    fix

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { fetchData } from "../../helpers/api-fetch";
 import { getSession } from "next-auth/client";
 import MessagesList from "../../components/messages/MessagesList";
+import axios from "axios";
 
 const Admin = ({ messages }) => {
   return (
@@ -25,7 +26,9 @@ export const getServerSideProps = async (context) => {
 
   let messages;
   try {
-    messages = await fetchData(`/api/contactHandler`);
+    messages = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/contactHandler`
+    );
   } catch (err) {
     console.log(err);
   }
