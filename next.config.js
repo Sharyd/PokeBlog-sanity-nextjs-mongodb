06b0674a65ae6b51e1a45bff77ba6901da7b0a8f
Daniel Vacek <Raimen77@seznam.cz>
2022-10-04 15:50:58 +0200

    fixed

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        MONGODB_USERNAME: "Dan",
+        MONGODB_PASSWORD: "lollol7",
+        MONGODB_CLUSTERNAME: "cluster0",
+        MONGODB_DATABASE: "my-poke-blog-dev",
       },
-    ];
-  },
+    };
+  }
+  return {
+    env: {
+      MONGODB_USERNAME: "Dan",
+      MONGODB_PASSWORD: "lollol7",
+      MONGODB_CLUSTERNAME: "cluster0",
+      MONGODB_DATABASE: "my-poke-blog",
+    },
+  };
 };
